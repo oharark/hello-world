669a729ecf00aefe6baa83771a55f7ba9f498938
oharark <69867815+oharark@users.noreply.github.com>
2020-08-18 14:15:19 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*******************************************
+*Hello.java
+********************************************
+*Rowan O'Hara
+*8/18/20
+*CMSC 255 - 004
+*******************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("<Your name>");
+		System.out.println("<Java Project Name>");
+		System.out.println("Project Description>");
+		System.out.println("<Version Date>");
+		System.out.println("<Course Number and Section>");
+		System.out.println();
+		}
+}
